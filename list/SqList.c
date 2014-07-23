0df51abecd70e4f23bbadb1488d62f7d75ae48d1
hejinlai <hejinlai@yunzhisheng.cn>
2014-07-23 11:48:24 +0800

    add list struct

--- list/SqList.c
+++ list/SqList.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LIST_INIT_SIZE 10
+#define LIST_INCREMENT 5
+
+typedef struct
+{
+    int *elem;
+    int length;
+    int listsize;
+}SqList;
 
 
 int main(int argc, char *argv[])
